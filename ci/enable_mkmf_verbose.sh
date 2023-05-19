8ba3c581599fb7ed9f48aeb4961c49dc97d26bac
Jun Aruga <jaruga@redhat.com>
2023-05-19 17:12:23 +0200

    Add GitHub Actions CI.
    Create a script to enable mkmf verbose.

---
+++ ci/enable_mkmf_verbose.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -eux
+
+mkmf_path="$(find "$(gem environment home)"/../.. -name mkmf.rb)"
+echo "mkmf_path: ${mkmf_path}"
+
+cp -p "${mkmf_path}" "${mkmf_path}.orig"
+sudo sed -i -e 's/^V = .*/V = 1/' "${mkmf_path}"
+
+grep '^V = ' "${mkmf_path}"
+diff "${mkmf_path}.orig" "${mkmf_path}" || :
