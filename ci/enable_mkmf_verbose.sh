67ac6dfa630750319e3a18216750081ee4f1a40a
Jun Aruga <jaruga@redhat.com>
2023-05-19 19:28:02 +0200

    Debug: print id and file permissions.

--- ci/enable_mkmf_verbose.sh
+++ ci/enable_mkmf_verbose.sh
@@ -5,6 +5,9 @@ set -eux
 mkmf_path="$(find "$(gem environment home)"/../.. -name mkmf.rb)"
 echo "mkmf_path: ${mkmf_path}"
 
+id
+ls -l "${mkmf_path}"
+
 cp -p "${mkmf_path}" "${mkmf_path}.orig"
 sudo sed -i -e 's/^V = .*/V = 1/' "${mkmf_path}"
 
