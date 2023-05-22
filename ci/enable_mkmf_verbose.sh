dea4f2778bb3e6464241f32adc71df5636117291
Jun Aruga <jaruga@redhat.com>
2023-05-22 20:03:50 +0200

    Refactor the script and CI yaml file.

--- ci/enable_mkmf_verbose.sh
+++ ci/enable_mkmf_verbose.sh
@@ -1,12 +1,13 @@
-#!/bin/bash
+#!/bin/sh -eu
 
-set -eux
+set -x
 
-mkmf_path="$(find "$(gem environment home)"/../.. -name mkmf.rb)"
-echo "mkmf_path: ${mkmf_path}"
+gem_home="$(gem environment home)"
+mkmf_path="$(find "${gem_home}"/../.. -name mkmf.rb)"
 
 cp -p "${mkmf_path}" "${mkmf_path}.orig"
-sudo sed -i -e 's/^V = .*/V = 1/' "${mkmf_path}"
 
-grep '^V = ' "${mkmf_path}"
+sed -i -e 's/^V = .*/V = 1/' "${mkmf_path}"
+grep '^V = 1' "${mkmf_path}"
+
 diff "${mkmf_path}.orig" "${mkmf_path}" || :
